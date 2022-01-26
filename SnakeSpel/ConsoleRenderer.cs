8e75b0363162ba86566413d7dc68b1da79bbcbb7
adtek98 <tekin360@gmail.com>
2022-01-26 14:46:13 +0100

    Add project files.

---
+++ SnakeSpel/ConsoleRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeSpel
+{
+    class ConsoleRenderer
+    {
+        private GameWorld world;
+        public ConsoleRenderer(GameWorld gameWorld)
+        {
+            // TODO Konfigurera Console-fönstret enligt världens storlek
+
+            world = gameWorld;
+        }
+
+        public void Render()
+        {
+            // TODO Rendera spelvärlden (och poängräkningen)
+
+            // Använd Console.SetCursorPosition(int x, int y) and Console.Write(char)
+        }
+    }
+}
